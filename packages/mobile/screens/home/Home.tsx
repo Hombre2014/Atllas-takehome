7991502d3c39757b23b5bafa71b19b0cba9b3a4f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-11-14 13:19:26 +0100

    Add NavigationButton component and refactoring the Home screen.

--- packages/mobile/screens/home/Home.tsx
+++ packages/mobile/screens/home/Home.tsx
@@ -1,51 +1,31 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackScreens } from '../../App';
 import { StatusBar } from 'expo-status-bar';
-import { useCallback } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { View } from 'react-native';
 
 import tw from 'twrnc';
+import NavigationButton from '../../components/NavigationButton';
 
 export default function Home({
   navigation,
 }: NativeStackScreenProps<StackScreens, 'Home'>) {
-  const handleLoginPress = useCallback(
-    () => navigation.navigate('Login'),
-    [navigation?.navigate]
-  );
-  const handleRegisterPress = useCallback(
-    () => navigation.navigate('Register'),
-    [navigation?.navigate]
-  );
-  const handleWebviewPress = useCallback(
-    () => navigation.navigate('App'),
-    [navigation?.navigate]
-  );
-
   return (
     <View style={tw`flex-1 justify-center items-center`}>
-      <TouchableOpacity
-        onPress={handleLoginPress}
-        style={tw`mb-4 bg-blue-500 p-2, rounded-lg w-1/2`}
-      >
-        <Text style={tw`text-white text-center font-bold text-lg`}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleRegisterPress}
-        style={tw`mb-4 bg-blue-500 p-2, rounded-lg w-1/2`}
-      >
-        <Text style={tw`text-white text-center font-bold text-lg`}>
-          Register
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleWebviewPress}
-        style={tw`mb-4 bg-blue-500 p-2, rounded-lg w-1/2`}
-      >
-        <Text style={tw`text-white text-center font-bold text-lg`}>
-          Skip to Webview
-        </Text>
-      </TouchableOpacity>
+      <NavigationButton
+        navigation={navigation}
+        destination="Login"
+        label="Login"
+      />
+      <NavigationButton
+        navigation={navigation}
+        destination="Register"
+        label="Register"
+      />
+      <NavigationButton
+        navigation={navigation}
+        destination="App"
+        label="Skip to Webview"
+      />
       <StatusBar style="auto" />
     </View>
   );
